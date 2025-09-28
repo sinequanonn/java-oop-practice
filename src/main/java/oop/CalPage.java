ef97277061637a67e8ef7f94b8932b4b1f09c031
sinequanonn <dleck288@gmail.com>
2025-09-29 06:41:34 +0900

    feat: 각 페이지 자릿수 합과 곱 계산 기능 구현
    각 페이지 자릿수 합 계산하는 메서드 구현
    각 페이지 자릿수 곱 계산하는 메서드 구현

---
+++ src/main/java/oop/CalPage.java
@@ -0,0 +1,25 @@
+package oop;
+
+public class CalPage {
+
+    // 페이지의 각 자릿수의 합을 계산하는 함수
+    public int sumPage(int page) {
+        int result = 0;
+        while (page != 0) {
+            result += (page % 10);
+            page /= 10;
+        }
+        return result;
+    }
+
+
+    public int mulPage(int page) {
+        int result = 1;
+
+        while (page != 0) {
+            result *= (page%10);
+            page /= 10;
+        }
+        return result;
+    }
+}
