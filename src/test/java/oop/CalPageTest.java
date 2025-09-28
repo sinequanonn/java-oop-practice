9a1e103ebbbe4cc67150d5d11668aa9f6a8922e3
sinequanonn <dleck288@gmail.com>
2025-09-29 06:41:54 +0900

    test: 각 페이지 자릿수 합과 곱 계산 테스트
    각 페이지 자릿수 합과 곱 계산 테스트 수행

---
+++ src/test/java/oop/CalPageTest.java
@@ -0,0 +1,35 @@
+package oop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+
+class CalPageTest {
+
+    @Test
+    void 페이지_각_자릿수_합_계산() {
+        //given
+        CalPage calPage = new CalPage();
+        int page = 97;
+
+        //when
+        int result = calPage.sumPage(page);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(16);
+    }
+
+    @Test
+    void 페이지_각_자릿수_곱_계산() {
+        //given
+        CalPage calPage = new CalPage();
+        int page = 97;
+
+        //when
+        int result = calPage.mulPage(page);
+
+        //then
+        Assertions.assertThat(result).isEqualTo(63);
+    }
+}
