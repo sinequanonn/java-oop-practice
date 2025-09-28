76511afb2cb148442222a5ffa19fde7d35ad0a44
sinequanonn <dleck288@gmail.com>
2025-09-29 07:22:17 +0900

    feat: 페이지 각 자릿수 계산 중 가장 큰 값 구하기
    페이지 각 자릿수 합과 곱 중 가장 큰 값 반환하기

---
+++ src/main/java/oop/User.java
@@ -0,0 +1,38 @@
+package oop;
+
+import java.util.List;
+
+public class User {
+    private String name;
+    private List<Integer> page;
+    private CalPage calPage;
+
+    public User(String name, List<Integer> page) {
+        this.name = name;
+        this.page = page;
+    }
+
+    public User(String name, List<Integer> page, CalPage calPage) {
+        this.name = name;
+        this.page = page;
+        this.calPage = calPage;
+    }
+
+    public int checkNumber() {
+        int result = 0;
+
+        // 왼쪽 페이지 계산
+        int left1 = calPage.sumPage(page.get(0));
+        result = Math.max(result, left1);
+        int left2 = calPage.mulPage(page.get(0));
+        result = Math.max(result, left2);
+
+        // 오른쪽 페이지 계산
+        int right1 = calPage.sumPage(page.get(0));
+        result = Math.max(result, right1);
+        int right2 = calPage.mulPage(page.get(1));
+        result = Math.max(result, right2);
+
+        return result;
+    }
+}
