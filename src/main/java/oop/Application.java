c286eed6f6d009ac82c2dfac19ccf575a6d4fc17
sinequanonn <dleck288@gmail.com>
2025-09-29 07:38:47 +0900

    feat: 전체 기능 구현 통합
    전체 기능 구현 통합

---
+++ src/main/java/oop/Application.java
@@ -0,0 +1,24 @@
+package oop;
+
+import java.util.Arrays;
+
+public class Application {
+
+    private static void solution() {
+        EvalResult evalResult = new EvalResult();
+
+        User pobi = new User("pobi", Arrays.asList(97, 98));
+        User crong = new User("crong", Arrays.asList(197, 198));
+
+        int pobi_result = pobi.checkNumber();
+        int crong_result = crong.checkNumber();
+
+        int result = evalResult.checkResult(pobi_result, crong_result);
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        // TODO 구현
+        solution();
+    }
+}
