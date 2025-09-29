635ec191c0cfa4faf1daaa4fcccde604f919934c
sinequanonn <dleck288@gmail.com>
2025-09-29 20:11:03 +0900

    refactor: User와 CalPage 의존성 느슨하게 변경
    CalPage 구체 클래스를 User에 생성한 것을 checkNumber 매개인자로 넘기도록 변경

--- src/main/java/oop/practice1/Application.java
+++ src/main/java/oop/practice1/Application.java
@@ -6,15 +6,15 @@ public class Application {
 
     private static void solution() {
         EvalResult evalResult = new EvalResult();
+        CalPage calPage = new CalPage();
 
         User pobi = new User("pobi", Arrays.asList(97, 98));
         User crong = new User("crong", Arrays.asList(197, 198));
 
-        int pobi_result = pobi.checkNumber();
-        int crong_result = crong.checkNumber();
+        int pobi_result = pobi.checkNumber(calPage);
+        int crong_result = crong.checkNumber(calPage);
 
-        int result = evalResult.checkResult(pobi_result, crong_result);
-        System.out.println(result);
+        evalResult.checkResult(pobi_result, crong_result);
     }
 
     public static void main(String[] args) {
--- src/main/java/oop/practice1/User.java
+++ src/main/java/oop/practice1/User.java
@@ -5,16 +5,14 @@ import java.util.List;
 public class User {
     private String name;
     private List<Integer> page;
-    private CalPage calPage = new CalPage();
 
     public User(String name, List<Integer> page) {
         this.name = name;
         this.page = page;
     }
 
-    public int checkNumber() {
+    public int checkNumber(CalPage calPage) {
         int result = 0;
-
         // 왼쪽 페이지 계산
         int left1 = calPage.sumPage(page.get(0));
         result = Math.max(result, left1);
