5b25b9c00b64c1ebb539cbff001e0d9b46076cf5
sinequanonn <dleck288@gmail.com>
2025-09-29 20:15:15 +0900

    refactor: 페이지 검증 로직 추가
    페이지 검증하는 로직 추가

--- src/main/java/oop/practice1/Application.java
+++ src/main/java/oop/practice1/Application.java
@@ -1,20 +1,28 @@
 package oop.practice1;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class Application {
 
     private static void solution() {
         EvalResult evalResult = new EvalResult();
         CalPage calPage = new CalPage();
+        ValPage valPage = new ValPage();
 
-        User pobi = new User("pobi", Arrays.asList(97, 98));
-        User crong = new User("crong", Arrays.asList(197, 198));
+        List<Integer> pobi_page = Arrays.asList(97, 98);
+        List<Integer> crong_page = Arrays.asList(197, 198);
+        valPage.validate(pobi_page);
+        valPage.validate(crong_page);
+
+        User pobi = new User("pobi", pobi_page);
+        User crong = new User("crong", crong_page);
 
         int pobi_result = pobi.checkNumber(calPage);
         int crong_result = crong.checkNumber(calPage);
 
-        evalResult.checkResult(pobi_result, crong_result);
+        int result = evalResult.checkResult(pobi_result, crong_result);
+        System.out.println(result);
     }
 
     public static void main(String[] args) {
